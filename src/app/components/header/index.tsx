13ba5d9dd59f720cf5bed00f469e5d3220c1db82
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-02-14 00:42:48 +0400

    feat: add header markup

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FirstHeaderSection from './firstHeaderSection'
+import SecondHeaderSection from './secondHeaderSection'
+import ThirdHeaderSection from './thirdHeaderSection'
+
+export default function Header() {
+  return (
+    <>
+    <FirstHeaderSection />
+    <SecondHeaderSection />
+    <ThirdHeaderSection />
+    </>
+  )
+}
---
+++ src/app/components/header/secondHeaderSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SecondHeaderSection() {
+  return (
+    <div className='w-full bg-[#32333E] px-[6%] flex  items-center justify-between py-[22px] gap-[20px] sm:hidden'>
+    <p className='text-[16px] text-white font-light'>ბანკის შესახებ</p>
+    <p className='text-[16px] text-white font-medium'>მონეტარული პოლიტიკა</p>
+    <p className='text-[16px] text-white font-light'>ფინანსური სტაბილურობა</p>
+    <p className='text-[16px] text-white font-light'>ზედამხედველობა</p>
+    <p className='text-[16px] text-white font-light'>ქართული ფული</p>
+    <p className='text-[16px] text-white font-light'>საგადახდო სისტემები</p>
+    <p className='text-[16px] text-white font-light'>სტატისტიკა</p>
+    <p className='text-[16px] text-white font-light'>პუბლიკაციები</p> 
+  </div>
+
+  )
+}
