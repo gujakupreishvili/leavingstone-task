48d504fa21a05291ad20b4347d333bf0034fafe7
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-02-17 22:27:14 +0400

    fix:dispatch data get funcs

--- src/app/components/main/index.tsx
+++ src/app/components/main/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import BlogImg from './blogImg'
-import Pinsection from './pinSection/pinSection'
-import FilterSection from './filterSection'
-import Info from './info'
+import React, { useEffect } from "react";
+import FilterSection from "./filterSection";
+import Info from "./info";
+import {  useDispatch,  } from "react-redux";
+import { getBlogs } from "@/app/store/blogs";
+import { AppDispatch } from "@/app/store/configureStore";
+import { getAuthorsNames } from "@/app/store/authors";
+import { getCategories } from "@/app/store/categories";
+import BlogImg from "./blogImg";
+import Pinsection from "./pinSection/pinSection";
+
+const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default function Main() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getBlogs("1", "10"));
+    dispatch(getAuthorsNames("1", "10"));
+    dispatch(getCategories("1", "10"));
+  }, [dispatch]);
   return (
-  <>
-  <BlogImg />
-  <Pinsection />
-  <FilterSection />
-  <Info />
-  </>
-  )
+    <>
+      <BlogImg/>
+      <Pinsection />
+      <FilterSection />
+      <Info />
+    </>
+  );
 }
