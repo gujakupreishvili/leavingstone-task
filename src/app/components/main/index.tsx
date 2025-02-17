b70a6150ec04bafebd409b9db6d2745807a2abc0
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-02-17 21:23:42 +0400

    feat/create blogImg and pinInfo

---
+++ src/app/components/main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogImg from './blogImg'
+import Pinsection from './pinSection/pinSection'
+
+export default function Main() {
+  return (
+  <>
+  <BlogImg />
+  <Pinsection />
+  </>
+  )
+}
